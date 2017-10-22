16b64a0770344b50a61624c043d800f27467ee23
Ljubov Deribasko <sto1000kvartir@gmail.com>
2017-10-22 15:32:05 +0300

    'tasks07'

---
+++ task_07_02_var.py
@@ -0,0 +1,31 @@
+"""
+Реализовать генератор случайных паролей указанной длины.
+
+В пароле можно использовать любые символы в верхнем и нижнем регистре.
+
+Например: password_generator(16), вернет случайный пароль длиной 16 символов.
+
+Пригодится стандартный модуль random
+
+Имя файла
+task_07_02.py
+Имя функции-генератора
+password_generator
+Возвращаемое значение
+Генератор
+
+"""
+
+
+import random
+from string import digits, ascii_letters
+
+
+def password_generator(n):
+    listing = list(digits + ascii_letters)
+    parol = [random.choice(listing) for i in range(n)]
+    parol = "".join(parol)
+    return parol
+
+
+#print(password_generator(16))
